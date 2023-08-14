4a120fedc036d7d6532675769ed6af4ad2889cac
Shreyash <shreyashnandurkar2@gmail.com>
2023-08-14 15:39:14 +0530

    changed no.

--- selection sortl.py
+++ selection sortl.py
@@ -2,13 +2,13 @@ def SelectionSort(l):
     for start in range(len(l)):
         minpos = start
 
-        for i in range(start+1, len(l)):
+        for i in range(start, len(l)):
             if l[i] < l[minpos]:
                 minpos = i
 
         l[start], l[minpos] = l[minpos], l[start]
 
 
-l = [121, 5, 34, 82, 582, 24, 512, 65, 78]
+l = [121, -5, 34, 82, 582, -24, -512, 65, -78]
 SelectionSort(l)
 print(l)
